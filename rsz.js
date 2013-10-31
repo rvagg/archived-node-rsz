3f89eb0cf0904ed5878bab8ba8700115f0419738
Tim <secoif@gmail.com>
2013-10-31 17:14:53 +0800

    Whitespace. Keep @rvagg happy.

--- rsz.js
+++ rsz.js
@@ -2,20 +2,20 @@ const fs     = require('fs')
     , Canvas = require('canvas')
 
 module.exports = function (src, width, height, dst, callback) {
-  var image     = new Canvas.Image()
-    , _oo       = typeof width == 'object'
-    , _width    = _oo ? width.width  : width
-    , _height   = _oo ? width.height : height
+  var image            = new Canvas.Image()
+    , _oo              = typeof width == 'object'
+    , _width           = _oo ? width.width  : width
+    , _height          = _oo ? width.height : height
     , _maintainRatio   = _oo ? width.aspectRatio : false
-    , _jpeg     = _oo ? width.type == 'jpeg' : false
-    , _dst      = typeof (_oo ? height : dst) == 'string'
+    , _jpeg            = _oo ? width.type == 'jpeg' : false
+    , _dst             = typeof (_oo ? height : dst) == 'string'
           ? (_oo ? height : dst)
           : null
     , _callback = typeof height == 'function'
           ? height
           : typeof dst == 'function' ? dst : callback
 
-    , write  = function (err, buf) {
+    , write = function (err, buf) {
         if (err)
           return _callback(err)
         fs.writeFile(_dst, buf, _callback)
